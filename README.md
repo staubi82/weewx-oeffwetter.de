0797f62a0e417d85cbf6c5e5ae9a5dc9253b5af5
staubi82 <56784126+staubi82@users.noreply.github.com>
2024-06-24 17:53:39 +0200

    install script test

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Debian Server:
 ```bash
 wget -qO - https://oeffwetter.de/install.sh | bash
 ```
+
+```http
+wget -qO - https://raw.githubusercontent.com/staubi82/weewx/main/install.sh | bash
+```
+
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# System aktualisieren und notwendige Pakete installieren
+apt update && apt -y upgrade && apt -y install wget debconf-utils locales git
+apt install python3-ephem python3-pcapy unzip gnupg gpg python3-paho-mqtt nginx -y
+
+# Deutsche Sprachunterstützung installieren
+echo "de_DE.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+update-locale LANG=de_DE.UTF-8
+
+# Überprüfen und setzen der Zeitzone auf Europe/Berlin
+CURRENT_TZ=$(timedatectl show --property=Timezone --value)
+if [ "$CURRENT_TZ" != "Europe/Berlin" ]; then
+    timedatectl set-timezone Europe/Berlin
+fi
+
+# Weewx GPG-Schlüssel hinzufügen und Repository einrichten
+wget -qO - https://weewx.com/keys.html | gpg --dearmor --output /etc/apt/trusted.gpg.d/weewx.gpg
+echo "deb [arch=all] https://weewx.com/apt/python3 buster main" | tee /etc/apt/sources.list.d/weewx.list
+
+# Weewx installieren mit Standardwerten
+DEBIAN_FRONTEND=noninteractive apt update && DEBIAN_FRONTEND=noninteractive apt install weewx -y
+
+# GW1000 Erweiterung installieren
+wget https://github.com/gjr80/weewx-gw1000/releases/download/v0.6.0/gw1000.zip
+weectl extension install gw1000.zip -y
+
+# Belchertown Erweiterung installieren
+wget https://github.com/poblabs/weewx-belchertown/releases/download/weewx-belchertown-1.3.1/weewx-belchertown-release.1.3.1.tar.gz
+weectl extension install weewx-belchertown-release.1.3.1.tar.gz -y
+
+# MQTT Erweiterung installieren
+wget -O weewx-mqtt.zip https://github.com/matthewwall/weewx-mqtt/archive/master.zip
+weectl extension install weewx-mqtt.zip -y
+
+# Temporäres Verzeichnis erstellen
+mkdir /tmp/weewx-config
+
+# Repository klonen (über HTTPS)
+git clone https://github.com/staubi82/weewx.git /tmp/weewx-config
+
+# Überprüfen, ob das Klonen erfolgreich war
+if [ $? -ne 0 ]; then
+    echo "Fehler beim Klonen des Repositories"
+    exit 1
+fi
+
+# Dateien kopieren
+cp -r /tmp/weewx-config/* /etc/weewx/
+
+# Überprüfen, ob das Kopieren erfolgreich war
+if [ $? -ne 0 ]; then
+    echo "Fehler beim Kopieren der Dateien"
+    exit 1
+fi
+
+# Temporäres Verzeichnis löschen
+rm -rf /tmp/weewx-config
+
+# Weewx-Dienst neu starten und Status anzeigen
+systemctl restart weewx
+systemctl status weewx
+
+echo "Konfigurationsdateien erfolgreich kopiert und Weewx wurde neu gestartet."
